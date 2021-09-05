c7745cdce9f90f1bc69a8c18edbcfff6fb28d4df
iikim <iikim2511@gmail.com>
2021-09-04 23:13:42 -0400

    css

---
+++ script.js
@@ -0,0 +1,39 @@
+(function () {
+
+    const stageElem = document.querySelector(".stage");
+    const houseElem = document.querySelector(".house");
+    const barElem = document.querySelector(".progress-bar");
+    const selectCharacterElem = document.querySelector(".selet-character");
+
+
+    const mousePos = {x:0, y:0};
+
+    let maxScrollValue;
+    console.log(pageYOffset);
+
+    // 410, 420 scroll, z-index
+
+    window.addEventListener("scroll",function (params) {
+        // const scrollPer = pageYOffset  / maxScrollValue;
+
+        // const zMove = scrollPer
+        
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+})();
